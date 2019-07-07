947f6271fcf99923af077d5ed8bdcfa1ff687b84
Bernadette Davis <bsd231@nyu.edu>
2019-07-07 15:40:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
     @artist = artist
     @genre = genre
 
-    @@count = 0
-    @@artists = []
-    @@genres = []
+    :@@count = 0
+    :@@artists = []
+    :@@genres = []
 
   end
 end
