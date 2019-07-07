e365a213e2db84b6854e815cfa2a44b10fd296b0
Bernadette Davis <bsd231@nyu.edu>
2019-07-07 15:40:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-
+  :@@count = 0
+  :@@artists = []
+  :@@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
 
-    :@@count = 0
-    :@@artists = []
-    :@@genres = []
+    
 
   end
 end
